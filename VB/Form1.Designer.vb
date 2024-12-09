4cdf1c0fdcfcf5f008e47bd9f07f65220bfac879
DevExpressExampleBot <fakeEmail>
2024-12-09 13:11:58 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -19,7 +19,7 @@ Namespace RichUsingSearchAPI
             MyBase.Dispose(disposing)
         End Sub
 
-'#Region "Windows Form Designer generated code"
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -60,7 +60,7 @@ Namespace RichUsingSearchAPI
             Me.ResumeLayout(False)
         End Sub
 
-'#End Region
+#End Region
         Private richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
 
         Private button1 As System.Windows.Forms.Button
